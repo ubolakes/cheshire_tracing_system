b3c803d48a74c0d0bb3b322ee91a097c99928013
UMBERTO LAGHI <ul2509@gmail.com>
2025-04-15 12:49:05 +0200

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,8 @@
 
 export PATH=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0/bin:$PATH
 
+export PATH=/usr/pack/gcc-11.2.0-af/linux-x64/bin:$PATH
+
+export PATH=/usr/scratch/kneiff/paulsc/opt/Python-3.8.3/bin:$PATH
+
 export RISCV=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0
